52e5ae8322d058378dd242f0d2a675ac7ee28dc3
Deneb Chorny <denebchorny@gmail.com>
2020-11-25 22:13:27 -0300

    [ADD] Roll button action

--- app/src/main/java/com/denebchorny/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/denebchorny/android/diceroller/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.denebchorny.android.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatTextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var tvResult: AppCompatTextView
+    private lateinit var btnRoll: AppCompatButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvResult = findViewById(R.id.tvResult)
+        btnRoll = findViewById(R.id.btnRoll)
+
+        btnRoll.setOnClickListener { onRollDice() }
     }
+
+    private fun onRollDice() = Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
 }
