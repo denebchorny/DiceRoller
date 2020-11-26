b79b5c029d398dc8d639f60a0fd00f7462062e84
Deneb Chorny <denebchorny@gmail.com>
2020-11-25 22:37:02 -0300

    [UPDATE] Replace dice roller result text with images

--- app/src/main/java/com/denebchorny/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/denebchorny/android/diceroller/MainActivity.kt
@@ -3,19 +3,19 @@ package com.denebchorny.android.diceroller
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
-import androidx.appcompat.widget.AppCompatTextView
+import androidx.appcompat.widget.AppCompatImageView
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var tvResult: AppCompatTextView
+    private lateinit var ivDice: AppCompatImageView
     private lateinit var btnRoll: AppCompatButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        tvResult = findViewById(R.id.tvResult)
+        ivDice = findViewById(R.id.ivDice)
         btnRoll = findViewById(R.id.btnRoll)
 
         btnRoll.setOnClickListener { onRollDice() }
@@ -23,6 +23,18 @@ class MainActivity : AppCompatActivity() {
 
     private fun onRollDice() {
         val randomNumber = Random.nextInt(6) + 1
-        tvResult.text = randomNumber.toString()
+        ivDice.setImageLevel(randomNumber)
+
+        // Course version
+       /*val drawableResource = when (randomNumber) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        ivDice.setImageResource(drawableResource)*/
     }
 }
